fe9dc0191b9f9395336f1da84e297b5332ec3ddc
Marcin Pyrka <marcin.pyrka@polcode.net>
2018-07-28 10:25:05 +0200

    CC

--- application/src/DataFixtures/UserFixtures.php
+++ application/src/DataFixtures/UserFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -9,9 +10,13 @@ class UserFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        for ($i = 0; $i < 2000; $i++) {
+            $user = new User();
+            $user->setName('name-' . $i);
+            $user->setLogin('login-' . $i);
+            $user->setEmail('email-' . $i . '@ubuntu');
+            $manager->persist($user);
+        }
         $manager->flush();
     }
 }
