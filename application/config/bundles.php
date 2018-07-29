8cb452f0ac076a695e49756838a9b29e7d95aa3d
Marcin Pyrka <pyrka.marcin@gmail.com>
2018-07-29 10:13:34 +0200

    Update

--- application/config/bundles.php
+++ application/config/bundles.php
@@ -13,4 +13,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Snc\RedisBundle\SncRedisBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
